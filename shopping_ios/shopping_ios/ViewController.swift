cca56f047c12b64a2e9384cb7e9068027ba2cf4f
shenpengfei <rg20china@gmail.com>
2015-09-29 22:48:31 +0800

    初始化shopping iOS

---
+++ shopping_ios/shopping_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shopping_ios
+//
+//  Created by shenpengfei on 15/9/29.
+//  Copyright (c) 2015年 malls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
