0811de1c45bf3ad5406a6173ee1a7243db534528
shenpengfei <rg20china@gmail.com>
2015-10-01 13:45:32 +0800

    增加了购物清单、添加商品的view。

---
+++ shopping_ios/shopping_ios/NewItemViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+class NewItemViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
--- shopping_ios/shopping_ios/ViewController.swift
+++ shopping_ios/shopping_ios/ViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
 
+    @IBOutlet weak var mytableView: UITableView!
+    
+    let categoryList = [("牛奶","三元"),("方便面","康师傅"),("巧克力","德芙"),
+        ("洗发水","飘柔"),("地板净","滴露"),("果汁","汇源"),("饮料","可口可乐"),("电器","海尔")];
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +25,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    //实现UITableViewDataSource中必须的继承方法区域
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return categoryList.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        var cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: "Cell");
+        
+        let (item,brand) = categoryList[indexPath.row]
+        cell.textLabel?.text = item
+        cell.detailTextLabel?.text = brand
+        //cell.textLabel?.text = "牛奶";
+        return cell;
+        
+    }
+
 
 
 }
